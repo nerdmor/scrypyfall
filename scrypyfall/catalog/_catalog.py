12f574fdca873ca58538720e13a276e6bfde5209
Felipe Proto <nerdmor@gmail.com>
2024-01-12 11:54:07 -0300

    improved catalog methods

--- scrypyfall/catalog/_catalog.py
+++ scrypyfall/catalog/_catalog.py
@@ -1,133 +1,150 @@
-from typing import NewType
+from typing import Any, NewType
 
 # Custom libraries
 from scrypyfall.foundation import ScrypyfallFoundation
-from scrypyfall.foundation import ScrypyfallCollection # TODO: remove this
 
 ScrypyfallCatalog = NewType('ScrypyfallCatalog', list)
 
 
 class Catalog():
+    """Generic wrapper for the /catalog/* endpoints
+    """
     def __init__(self) -> None:
-        # TODO: add header updating
         self.dummy_foundation = ScrypyfallFoundation('catalog')
+    
+    def __call__(self, endpoint:str, **kwargs: Any) -> ScrypyfallCatalog:
+        """Calls a given catalog endpoint
+        
+        This is a wrapper for all the internal, endpoint-specific, named
+        methods
 
-    def card_names(self) -> ScrypyfallCatalog:
+        Args:
+            endpoint (str): endpoint to be called
+
+        Returns:
+            ScrypyfallCatalog: a IterableResponse-derived object
+        """
+        endpoint = endpoint.lower().strip().replace('-', '_')
+        func = getattr(self, endpoint)
+        return func(**kwargs)
+        
+
+    def card_names(self, **kwargs) -> ScrypyfallCatalog:
         """Wrapper for the /catalog/card-names endpoint
 
         Returns:
             ScrypyfallCatalog: a IterableResponse-derived object
         """
-        return self.dummy_foundation.make_request('card-names')
-    def artist_names(self) -> ScrypyfallCatalog:
+        return self.dummy_foundation.make_request('card-names', **kwargs)
+    def artist_names(self, **kwargs) -> ScrypyfallCatalog:
         """Wrapper for the /catalog/artist-names endpoint
 
         Returns:
             ScrypyfallCatalog: a IterableResponse-derived object
         """
-        return self.dummy_foundation.make_request('artist-names')
-    def word_bank(self) -> ScrypyfallCatalog:
+        return self.dummy_foundation.make_request('artist-names', **kwargs)
+    def word_bank(self, **kwargs) -> ScrypyfallCatalog:
         """Wrapper for the /catalog/word-bank endpoint
 
         Returns:
             ScrypyfallCatalog: a IterableResponse-derived object
         """
-        return self.dummy_foundation.make_request('word-bank')
-    def creature_types(self) -> ScrypyfallCatalog:
+        return self.dummy_foundation.make_request('word-bank', **kwargs)
+    def creature_types(self, **kwargs) -> ScrypyfallCatalog:
         """Wrapper for the /catalog/creature-types endpoint
 
         Returns:
             ScrypyfallCatalog: a IterableResponse-derived object
         """
-        return self.dummy_foundation.make_request('creature-types')
-    def planeswalker_types(self) -> ScrypyfallCatalog:
+        return self.dummy_foundation.make_request('creature-types', **kwargs)
+    def planeswalker_types(self, **kwargs) -> ScrypyfallCatalog:
         """Wrapper for the /catalog/planeswalker-types endpoint
 
         Returns:
             ScrypyfallCatalog: a IterableResponse-derived object
         """
-        return self.dummy_foundation.make_request('planeswalker-types')
-    def land_types(self) -> ScrypyfallCatalog:
+        return self.dummy_foundation.make_request('planeswalker-types', **kwargs)
+    def land_types(self, **kwargs) -> ScrypyfallCatalog:
         """Wrapper for the /catalog/land-types endpoint
 
         Returns:
             ScrypyfallCatalog: a IterableResponse-derived object
         """
-        return self.dummy_foundation.make_request('land-types')
-    def artifact_types(self) -> ScrypyfallCatalog:
+        return self.dummy_foundation.make_request('land-types', **kwargs)
+    def artifact_types(self, **kwargs) -> ScrypyfallCatalog:
         """Wrapper for the /catalog/artifact-types endpoint
 
         Returns:
             ScrypyfallCatalog: a IterableResponse-derived object
         """
-        return self.dummy_foundation.make_request('artifact-types')
-    def enchantment_types(self) -> ScrypyfallCatalog:
+        return self.dummy_foundation.make_request('artifact-types', **kwargs)
+    def enchantment_types(self, **kwargs) -> ScrypyfallCatalog:
         """Wrapper for the /catalog/enchantment-types endpoint
 
         Returns:
             ScrypyfallCatalog: a IterableResponse-derived object
         """
-        return self.dummy_foundation.make_request('enchantment-types')
-    def spell_types(self) -> ScrypyfallCatalog:
+        return self.dummy_foundation.make_request('enchantment-types', **kwargs)
+    def spell_types(self, **kwargs) -> ScrypyfallCatalog:
         """Wrapper for the /catalog/spell-types endpoint
 
         Returns:
             ScrypyfallCatalog: a IterableResponse-derived object
         """
-        return self.dummy_foundation.make_request('spell-types')
-    def powers(self) -> ScrypyfallCatalog:
+        return self.dummy_foundation.make_request('spell-types', **kwargs)
+    def powers(self, **kwargs) -> ScrypyfallCatalog:
         """Wrapper for the /catalog/powers endpoint
 
         Returns:
             ScrypyfallCatalog: a IterableResponse-derived object
         """
-        return self.dummy_foundation.make_request('powers')
-    def toughnesses(self) -> ScrypyfallCatalog:
+        return self.dummy_foundation.make_request('powers', **kwargs)
+    def toughnesses(self, **kwargs) -> ScrypyfallCatalog:
         """Wrapper for the /catalog/toughnesses endpoint
 
         Returns:
             ScrypyfallCatalog: a IterableResponse-derived object
         """
-        return self.dummy_foundation.make_request('toughnesses')
-    def loyalties(self) -> ScrypyfallCatalog:
+        return self.dummy_foundation.make_request('toughnesses', **kwargs)
+    def loyalties(self, **kwargs) -> ScrypyfallCatalog:
         """Wrapper for the /catalog/loyalties endpoint
 
         Returns:
             ScrypyfallCatalog: a IterableResponse-derived object
         """
-        return self.dummy_foundation.make_request('loyalties')
-    def watermarks(self) -> ScrypyfallCatalog:
+        return self.dummy_foundation.make_request('loyalties', **kwargs)
+    def watermarks(self, **kwargs) -> ScrypyfallCatalog:
         """Wrapper for the /catalog/watermarks endpoint
 
         Returns:
             ScrypyfallCatalog: a IterableResponse-derived object
         """
-        return self.dummy_foundation.make_request('watermarks')
-    def keyword_abilities(self) -> ScrypyfallCatalog:
+        return self.dummy_foundation.make_request('watermarks', **kwargs)
+    def keyword_abilities(self, **kwargs) -> ScrypyfallCatalog:
         """Wrapper for the /catalog/keyword-abilities endpoint
 
         Returns:
             ScrypyfallCatalog: a IterableResponse-derived object
         """
-        return self.dummy_foundation.make_request('keyword-abilities')
-    def keyword_actions(self) -> ScrypyfallCatalog:
+        return self.dummy_foundation.make_request('keyword-abilities', **kwargs)
+    def keyword_actions(self, **kwargs) -> ScrypyfallCatalog:
         """Wrapper for the /catalog/keyword-actions endpoint
 
         Returns:
             ScrypyfallCatalog: a IterableResponse-derived object
         """
-        return self.dummy_foundation.make_request('keyword-actions')
-    def ability_words(self) -> ScrypyfallCatalog:
+        return self.dummy_foundation.make_request('keyword-actions', **kwargs)
+    def ability_words(self, **kwargs) -> ScrypyfallCatalog:
         """Wrapper for the /catalog/ability-words endpoint
 
         Returns:
             ScrypyfallCatalog: a IterableResponse-derived object
         """
-        return self.dummy_foundation.make_request('ability-words')
-    def supertypes(self) -> ScrypyfallCatalog:
+        return self.dummy_foundation.make_request('ability-words', **kwargs)
+    def supertypes(self, **kwargs) -> ScrypyfallCatalog:
         """Wrapper for the /catalog/supertypes endpoint
 
         Returns:
             ScrypyfallCatalog: a IterableResponse-derived object
         """
-        return self.dummy_foundation.make_request('supertypes')
+        return self.dummy_foundation.make_request('supertypes', **kwargs)
